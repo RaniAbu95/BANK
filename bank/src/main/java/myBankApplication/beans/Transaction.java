f0b4dbbd394c85cf7e5678da7bb7dd37342f451a
baselasli <baselasli48@hotmail.com>
2024-07-16 12:23:13 +0300

    creating transactionController add amount to class transaction

--- bank/src/main/java/myBankApplication/beans/Transaction.java
+++ bank/src/main/java/myBankApplication/beans/Transaction.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.Instant;
+import java.time.LocalDate;
 
 @Entity
 @Table(name="Transactions")
@@ -14,11 +15,13 @@ public class Transaction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int transactionId;
     @Column(name="TimeStamp")
-    private Instant timeStamp;
+    private String timeStamp;
     @Column(name="Operation")
     private String operation;
     @Column(name="Target")
     private String target;
+    @Column(name="Amount")
+    private int amount;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_id")
@@ -30,10 +33,12 @@ public class Transaction {
     public Transaction() {
     }
 
-    public Transaction( String target, String operation, Instant timeStamp) {
+    public Transaction( String target, String operation, String timeStamp,int amount,Account account) {
         this.target = target;
         this.operation = operation;
         this.timeStamp = timeStamp;
+        this.amount=amount;
+        this.account=account;
     }
 
 
@@ -61,11 +66,30 @@ public class Transaction {
         this.operation = operation;
     }
 
-    public Instant getTimeStamp() {
+    public String getTimeStamp() {
         return timeStamp;
     }
 
-    public void setTimeStamp(Instant timeStamp) {
+    public void setTimeStamp(String timeStamp) {
         this.timeStamp = timeStamp;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "transactionId=" + transactionId +
+                ", timeStamp=" + timeStamp +
+                ", operation='" + operation + '\'' +
+                ", target='" + target + '\'' +
+                ", account=" + account +
+                '}';
     }
 }
